8d93682551dded9be62c8fa61eb5615710875260
German Ulrich <gerulrich@gmail.com>
2025-06-01 18:23:54 -0300

    refactor: rename SECRET_KEY to key for consistency in JWT handling

--- api/src/helpers/jwt.js
+++ api/src/helpers/jwt.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-const SECRET_KEY = Buffer.from(process.env.JWT_SECRET_KEY, 'base64url');
+const key = Buffer.from(process.env.JWT_SECRET_KEY, 'base64url');
 
 const createJWT = (uid, roles = [], level = 1, expiresIn = 86400) => {
-  const payload = { uid, roles, level };
-  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+  const payload = { uid, roles, level, sub: uid, groups: roles, version: process.env.JWT_VERSION, kid: process.env.JWT_KID };
+  return jwt.sign(payload, key, { expiresIn });
 };
 
 const decodeJWT = (token = '') => {
-  const { uid, roles, level } = jwt.verify(token, SECRET_KEY);
+  const { uid, roles, level } = jwt.verify(token, key);
   return {
     uid, roles, level
   };
