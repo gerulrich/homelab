908098961cc8af771d5556e3365f43a07fbd8797
German Ulrich <gerulrich@gmail.com>
2025-06-02 00:24:47 -0300

    refactor: use Buffer for JWT_SECRET_KEY to enhance security

--- api/src/middlewares/validate-jwt.js
+++ api/src/middlewares/validate-jwt.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 
+const key = Buffer.from(process.env.JWT_SECRET_KEY, 'base64url');
+
 const validateJWT = (req, res, next) => {
   const token = req.header('Authorization');
   if (!token) {
     return res.status(401).json({ msg: 'Authorization header is required' });
   }
   try {
-    const { uid, roles, level } = jwt.verify(token.substring(7), process.env.JWT_SECRET_KEY);
+    const { uid, roles, level } = jwt.verify(token.substring(7), key);
     req.user = { uid, roles, level };
     next();
   } catch (error) { // eslint-disable-line no-unused-vars
